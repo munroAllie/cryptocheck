a29b7fe9ca93a8ce1a8b796278ce3490447d76cb
John Munro <John@Johns-MacBook-Pro.local>
2017-03-17 22:11:54 -0300

    Created a service

---
+++ src/app/Conversion.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import { Http } from '@angular/http';
+@Injectable()
+export class ConversionService{
+    constructor(public http: Http)
+    {
+        
+    }
+
+    GetData()
+    {
+    
+
+    }
+}
+  
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {ConversionService} from 'app/Conversion.service';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -14,7 +14,7 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [ConversionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
