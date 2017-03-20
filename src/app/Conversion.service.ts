8f9ac3139f71d1d8c9ceb03ea5087b84fb7c0662
John Munro <John@Johns-MacBook-Pro.local>
2017-03-19 21:11:27 -0300

    beginning files for the get conversion service

--- src/app/Conversion.service.ts
+++ src/app/Conversion.service.ts
@@ -1,16 +1,13 @@
 import {Injectable} from '@angular/core';
 import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class ConversionService{
-    constructor(public http: Http)
-    {
-        
-    }
-
-    GetData()
-    {
+    constructor(public http: Http) {}
     
-
+    getCurrentTime(){
+        return this.http.get('https://poloniex.com/public?command=returnTicker')
+            .map(res => res.json());
     }
 }
   
