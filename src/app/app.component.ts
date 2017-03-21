4abfefa056d392d08adabcbe1dead89a1fedc65a
John Munro <John@Johns-MacBook-Pro.local>
2017-03-20 21:02:00 -0300

    Cleans up pull requests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,31 @@ import {Ticker} from './TickerType'; //Imports the Model Ticker that holds the f
 export class AppComponent {
 
 
-  ticker: Ticker[]; //Will hold the data from the JSON file
-  
-  constructor(private conversionService: ConversionService){} //Creates a conversionService object so we can call the GetTickerMethod from the service.
+ ticker: Ticker[]; //Will hold the data from the JSON file
+ step2AOptions : any[];
+ step2BOptions : any[];
+ step2Selection: string;
+ holdings: number;
+ CAD: number = 1;
+ bitcoin: number = 1283.00;
+ 
+  constructor(private conversionService: ConversionService){
+    this.step2AOptions = [
+      {name: "make a selection..."},
+      {name: "Bitcoin"},
+      {name: "DASH"},
+      {name: "Etherium"}  
+    ]; // step2AOptions
+
+    this.step2BOptions = [
+      {name: "make a selection..."},
+      {name: "CAD"},
+      {name: "USD"} 
+    ]; // step2BOptions
+  } //Creates a conversionService object so we can call the GetTickerMethod from the service.
   
 
-  getCurrentTicker(name1:string,name2:string) {
+getCurrentTicker(name1:string,name2:string) {
   
     this.conversionService.getCurrentTicker() //Sends a request for the ticker information
                             .then(res => { this.ticker = res;
@@ -25,46 +44,6 @@ export class AppComponent {
                                          }//End the "res"" fuction      
                                   )//Ends the "then"function
   }//Ends the GetCurrentTicker Functions
-}//End AppComponent Class
-
-  time : String;
-  private step2AOptions : any[];
-  private step2BOptions : any[];
-  private step2Selection: string;
-  private conversionService: ConversionService;
-  private holdings: number;
-  private CAD: number = 1;
-  private bitcoin: number = 1283.00;
-
-  constructor(){
-
-    this.step2AOptions = [
-    {name: "make a selection..."},
-    {name: "Bitcoin"},
-    {name: "DASH"},
-    {name: "Etherium"}  
-  ]; // step2AOptions
-
-  this.step2BOptions = [
-    {name: "make a selection..."},
-    {name: "CAD"},
-    {name: "USD"} 
-  ]; // step2BOptions
-
-  } // constructor
-  getCurrentTime() {
-    this.conversionService.getCurrentTime()
-    .subscribe(
-     data => this.time = JSON.stringify(data), 
-     error => alert(error)
-      )  // subscribe
-  };  // get current time
-  NgOnChanges(){
-    console.log("I am changing!!");
-    if (this.step2Selection === this.step2AOptions[1]){
-      this.CAD = this.holdings * this.bitcoin;
-    } // bitcoin if
-  } //ng chnages
 } //export class
 
 
