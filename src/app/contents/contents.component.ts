2c600909bf060953f8cd3a47a6de203517343285
Allie Munro <allie.munro@smartsti.com>
2017-04-02 19:52:44 -0300

    makes title binding work with updating values

--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Ticker } from '../TickerType'; 
+import { Ticker } from '../TickerType';
 import { ConversionService } from '../Conversion.service';
-import {Observable} from 'rxjs/Rx';
-import {coinResultsType} from './resultsTypeInterface';
-import {ExchangeType} from './ExchangeTypeInterface';
-import { Title }     from '@angular/platform-browser';
+import { Observable } from 'rxjs/Rx';
+import { coinResultsType } from './resultsTypeInterface';
+import { ExchangeType } from './ExchangeTypeInterface';
+import { Title } from '@angular/platform-browser';
 import { WindowScrollService } from '../window-scroll.service';
 
 
@@ -13,157 +13,155 @@ import { WindowScrollService } from '../window-scroll.service';
   templateUrl: './contents.component.html',
   styleUrls: ['./contents.component.scss']
 })
-export class ContentsComponent implements OnInit{
+export class ContentsComponent implements OnInit {
 
-//Will hold the data from the JSON file
+  //Will hold the data from the JSON file
 
 
   // Variables for front end
 
 
- instructions: boolean = false;
- instructionsLabel: string = "What is this?";
- pageTitle: string = "Cryptocheck.io";
+  instructions: boolean = false;
+  instructionsLabel: string = "What is this?";
+  pageTitle: string = "Cryptocheck.io";
 
 
- cryptoSelected : boolean = false; //Determines if crypto is selected
- regSelected : boolean = false; // Determines if currenecy s selected
- 
- //ADD IN ANY EXTRA CURRENCIES OR COINS YOU WANT (ALSO ADD IT IN THE COINS AND CURRENCY ARRAY BELOW)
- //ALSO GO TO THE CONVERTNAME FUCTION AND ADD IN THE APPROPRIATE NAME (VERY BOTTOM OF FILE
- step2AOptions : any[] = [
-      {name: "make a selection..."},
-      {name: "DASH"},
-      {name: "Etherium"},
-      {name: "Bitcoin"}  
-    ]; // step2AOptions
- step2BOptions : any[] = [
-      {name: "make a selection..."},
-      {name: "CAD"},
-      {name: "USDT"} 
-    ]; // step2BOptions
+  cryptoSelected: boolean = false; //Determines if crypto is selected
+  regSelected: boolean = false; // Determines if currenecy s selected
+
+  //ADD IN ANY EXTRA CURRENCIES OR COINS YOU WANT (ALSO ADD IT IN THE COINS AND CURRENCY ARRAY BELOW)
+  //ALSO GO TO THE CONVERTNAME FUCTION AND ADD IN THE APPROPRIATE NAME (VERY BOTTOM OF FILE
+  step2AOptions: any[] = [
+    { name: "make a selection..." },
+    { name: "DASH" },
+    { name: "Etherium" },
+    { name: "Bitcoin" }
+  ]; // step2AOptions
+  step2BOptions: any[] = [
+    { name: "make a selection..." },
+    { name: "CAD" },
+    { name: "USDT" }
+  ]; // step2BOptions
 
 
 
- step2Selection: string; //Holds the value of the currency you have selected.
- holdings: number = 10; //The amount of money you have inputed.
+  step2Selection: string; //Holds the value of the currency you have selected.
+  holdings: number = 10; //The amount of money you have inputed.
 
-// DONT FORGET TO ADD THE COIN OR CURRENCY BELOW IN THE PROPER FORMAT (CHECK THE TICKER OR CURRENCY EXCHANGE FOR FORMAT)
-coins: any[] = ["BTC_ETH", "BTC_DASH", 'BTC_BTC' ]; //holds the coin as string in the format of the ticker name
-currencies: any[] = ["CAD", "EUR", 'USDT']; //hold the currencies as a string in the format of currencyExchange
-ticker: Ticker[]; // Holds the exchange values for the coins
-currencyExchange: ExchangeType[] = [];   //Holds the exchange values for the currencies
-coinResults: coinResultsType[] = []; //Holds all the names and converted values (DISPLAY THIS IN THE DOM)
+  // DONT FORGET TO ADD THE COIN OR CURRENCY BELOW IN THE PROPER FORMAT (CHECK THE TICKER OR CURRENCY EXCHANGE FOR FORMAT)
+  coins: any[] = ["BTC_ETH", "BTC_DASH", 'BTC_BTC']; //holds the coin as string in the format of the ticker name
+  currencies: any[] = ["CAD", "EUR", 'USDT']; //hold the currencies as a string in the format of currencyExchange
+  ticker: Ticker[]; // Holds the exchange values for the coins
+  currencyExchange: ExchangeType[] = [];   //Holds the exchange values for the currencies
+  coinResults: coinResultsType[] = []; //Holds all the names and converted values (DISPLAY THIS IN THE DOM)
 
 
-interval: any;
-navIsFixed: boolean;
-amountnew: number;
+  interval: any;
+  navIsFixed: boolean;
+  amountnew: number;
 
-increase(amount: number){
-  this.amountnew = amount +1.0;
-  console.log(amount);
-  console.log(this.amountnew);
-}
-decrease(amount: number){
-  this.amountnew = amount -1.0;
-  console.log(amount);
-  console.log(this.amountnew);
-}
-
-  constructor( private conversionService: ConversionService, private windowscrollservice: WindowScrollService, private titleService: Title ) { 
-    
+  increase(amount: number) {
+    this.amountnew = amount + 1.0;
+    console.log(amount);
+    console.log(this.amountnew);
   }
-    ngOnInit(){
-   this.interval = setInterval(() => {
-        this.windowscrollservice.onWindowScroll() ;
-        this.navIsFixed = this.windowscrollservice.navIsFixed;
+  decrease(amount: number) {
+    this.amountnew = amount - 1.0;
+    console.log(amount);
+    console.log(this.amountnew);
+  }
+
+  constructor(private conversionService: ConversionService, private windowscrollservice: WindowScrollService, private titleService: Title) {
+
+  }
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.windowscrollservice.onWindowScroll();
+      this.navIsFixed = this.windowscrollservice.navIsFixed;
     }, 10);
 
-      setInterval(()=> 
-      {
+    setInterval(() => {
 
-      
-      this.conversionService.getFullTicker().subscribe((res) => {this.ticker = res;
-      this.ticker['BTC_BTC'] = {
-                              id: 1,
-                              last: 1,
-                              lowestAsk: 1,
-                              highestBid: 1,
-                              percentChange: 1,
-                              baseVolume: 1,
-                              quoteVolume: 1,
-                              isFrozen: 1,
-                              high24hr: 1,
-                              low24hr: 1
-                               };
-      
+
+      this.conversionService.getFullTicker().subscribe((res) => {
+      this.ticker = res;
+        this.ticker['BTC_BTC'] = {
+          id: 1,
+          last: 1,
+          lowestAsk: 1,
+          highestBid: 1,
+          percentChange: 1,
+          baseVolume: 1,
+          quoteVolume: 1,
+          isFrozen: 1,
+          high24hr: 1,
+          low24hr: 1
+        };
+
       }); //end the subscribe function                                                       
-     this.conversionService.getFullCurrencyExchange().subscribe( (res) => {this.currencyExchange = res["rates"];
-     this.currencyExchange['USDT'] = 1;
-    })
+      this.conversionService.getFullCurrencyExchange().subscribe((res) => {
+      this.currencyExchange = res["rates"];
+        this.currencyExchange['USDT'] = 1;
+      })
       this.convert();//end subscribe fuction
-      },1000);
+    }, 1000);
 
-    }// End OnInit
+  }// End OnInit
 
-  
-    convert()
-    { 
-      
-      this.coinResults = [];
-      if(this.cryptoSelected)
-      {
-        //convert all the crypto to currencies
-        for (var i = 0; i<= this.currencies.length -1 ; i++)
-        {
-          var tempName = this.currencies[i] as string;
-          this.coinResults.push(
-            {name: this.convertName(tempName as string),
-            amount: this.holdings * this.ticker[this.convertName(this.step2Selection)].last * this.ticker['USDT_BTC'].last* this.currencyExchange[tempName]}
-          )
-        }
-        //convert all the crypto to crypto
-        for(var i = 0 ; i <= this.coins.length - 1; i++)
-        {
-          var tempName = this.coins[i] as string;
-          
-          this.coinResults.push(
-            {name: this.convertName(tempName as string), 
-             amount: this.holdings * this.ticker[this.convertName(this.step2Selection)].last / this.ticker[tempName].last
-           });  
-           
-        }
+
+  convert() {
+
+    this.coinResults = [];
+    if (this.cryptoSelected) {
+      //convert all the crypto to currencies
+      for (var i = 0; i <= this.currencies.length - 1; i++) {
+        var tempName = this.currencies[i] as string;
+        this.coinResults.push(
+          {
+            name: this.convertName(tempName as string),
+            amount: this.holdings * this.ticker[this.convertName(this.step2Selection)].last * this.ticker['USDT_BTC'].last * this.currencyExchange[tempName]
+          }
+        )
       }
-      if(this.regSelected)
-        {
-            //convert currency to currency
-            for (var i = 0; i<= this.currencies.length -1 ; i++)
-            {
-             var tempName = this.currencies[i] as string;
-            this.coinResults.push(
-                              {name: this.convertName(tempName as string),
-                               amount: this.holdings / this.currencyExchange[this.convertName(this.step2Selection)] * this.currencyExchange[tempName]
-                              })
-             }
-            
-             //convert currency to crypto
-            for(var i = 0 ; i <= this.coins.length - 1; i++)
-            {
-              var tempName = this.coins[i] as string;
-              this.coinResults.push(
-               {name: this.convertName(tempName as string), 
-                amount: this.holdings / this.currencyExchange[this.convertName(this.step2Selection)] / this.ticker['USDT_BTC'].last / this.ticker[tempName].last
+      //convert all the crypto to crypto
+      for (var i = 0; i <= this.coins.length - 1; i++) {
+        var tempName = this.coins[i] as string;
 
-           });  
-           
-        }
+        this.coinResults.push(
+          {
+            name: this.convertName(tempName as string),
+            amount: this.holdings * this.ticker[this.convertName(this.step2Selection)].last / this.ticker[tempName].last
+          });
 
-        } 
-    }// END CONVERT
-    convertName(name: string)
-    {
-      switch(name){
+      }
+    }
+    if (this.regSelected) {
+      //convert currency to currency
+      for (var i = 0; i <= this.currencies.length - 1; i++) {
+        var tempName = this.currencies[i] as string;
+        this.coinResults.push(
+          {
+            name: this.convertName(tempName as string),
+            amount: this.holdings / this.currencyExchange[this.convertName(this.step2Selection)] * this.currencyExchange[tempName]
+          })
+      }
+
+      //convert currency to crypto
+      for (var i = 0; i <= this.coins.length - 1; i++) {
+        var tempName = this.coins[i] as string;
+        this.coinResults.push(
+          {
+            name: this.convertName(tempName as string),
+            amount: this.holdings / this.currencyExchange[this.convertName(this.step2Selection)] / this.ticker['USDT_BTC'].last / this.ticker[tempName].last
+
+          });
+
+      }
+
+    }
+  }// END CONVERT
+  convertName(name: string) {
+    switch (name) {
       case 'Bitcoin':
         return ('BTC_BTC');
       case 'BTC_BTC':
@@ -171,12 +169,12 @@ decrease(amount: number){
 
 
       case 'DASH':
-        return('BTC_DASH');
+        return ('BTC_DASH');
       case 'BTC_DASH':
-        return('DASH');
+        return ('DASH');
 
       case 'Etherium':
-        return('BTC_ETH');
+        return ('BTC_ETH');
       case 'BTC_ETH':
         return ('Etherium');
 
@@ -185,36 +183,48 @@ decrease(amount: number){
       case 'CAD':
         return ('CAD');
       case 'EUR':
-        return('EUR');
+        return ('EUR');
 
-        }//END SWITCH
+    }//END SWITCH
 
-    }//END CONVERT
-    
-    pinnedCoinAmount: string ;
-    pinnedCoinName: string ;
+  }//END CONVERT
 
-    pinnedCoin: string ;
-    pinTitle(amount: number, name: string){
-      if(amount){
-      this.pinnedCoinAmount = amount.toString();
-      this.pinnedCoinName = name.toString();
-      this.pinnedCoin = this.pinnedCoinName + " - " + this.pinnedCoinAmount;
-      this.setTitle(this.pinnedCoin);
-      console.log(this.pinnedCoinAmount);
+  pinnedCoinAmount: number;
+  pinnedCoinName: string;
+  pinnedCoin: string;
+  updateTitle: any;
+
+  pinTitle(amount: number, name: string) {
+    clearInterval(this.updateTitle);
+    this.updateTitle = setInterval(()=>{
+      for(var i = 0; i<= this.coinResults.length; i++){
+        if(name == this.coinResults[i].name){
+          this.pinnedCoinAmount = this.coinResults[i].amount;
+          this.setTitle(this.coinResults[i].name + " - " + this.coinResults[i].amount.toString());
+        }
+      }
+    }, 100);
+  }
+ 
+
+
+    /* this.pinnedCoinAmount = amount.toString();
+     this.pinnedCoinName = name.toString();
+     this.pinnedCoin = this.pinnedCoinName + " - " + this.pinnedCoinAmount;
+     this.setTitle(this.pinnedCoin);
+     console.log(this.pinnedCoinAmount);*/
+  
+  setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
+  instructionsToggle() {
+    if (this.instructions) {
+      this.instructionsLabel = "Close Instructions";
+    }
+    else {
+      this.instructionsLabel = "What is this?";
     }
   }
-    setTitle( newTitle: string) {
-    this.titleService.setTitle( newTitle );
-  } 
-    instructionsToggle(){
-      if(this.instructions){
-        this.instructionsLabel = "Close Instructions";
-      }
-      else{
-        this.instructionsLabel = "What is this?";
-      }
-    }
 }
 
 
